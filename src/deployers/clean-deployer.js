d48b5e65546c535ec2409c23eb96884d3b7fda9f
Angelo Laub <angelolaub@gmail.com>
2019-06-18 12:34:07 +0200

    CleanDeployer allow staking of CPU, NET and RAM buying for the new account

--- src/deployers/clean-deployer.js
+++ src/deployers/clean-deployer.js
@@ -1,5 +1,5 @@
 const EOSDeployer = require('./eos-deployer');
-
+const assert = require('assert')
 const defineImmutableProperties = require('./../helpers/immutable-properties').defineImmutableProperties;
 
 class CleanDeployer extends EOSDeployer {
@@ -9,8 +9,20 @@ class CleanDeployer extends EOSDeployer {
         defineImmutableProperties(this, [
             {
                 name: 'deploy',
-                value: async function (wasmPath, abiPath) {
+                value: async function (wasmPath, abiPath, cpuAmount=null, netAmount=null, ramBytes=null, payer=provider.defaultAccount) {
                     let newContractAccount = await accountFactory.createRandom();
+                    if(cpuAmount) {
+                      assert(netAmount, "If cpuAmount is given, netAmount must be given as well")
+                    }
+                    if(netAmount) {
+                      assert(cpuAmount, "If netAmount is given, cpuAmount must be given as well")
+                    }
+                    if(cpuAmount && netAmount) {
+                      await newContractAccount.buyBandwidth(cpuAmount, netAmount, payer)
+                    }
+                    if(ramBytes) {
+                      await newContractAccount.buyRam(ramBytes, payer)
+                    }
                     return this.__deploy(wasmPath, abiPath, newContractAccount);;
                 }
             }
